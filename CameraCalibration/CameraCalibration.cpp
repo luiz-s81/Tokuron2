2f742d7b80faa981c8a9223f4e75d1d30a9ca47b
Luiz Gustavo M. Sampaio <lgmsampaio@gmail.com>
2014-07-07 11:39:59 +0900

    Reorganized the methods in Camera Calibration to not need to use headers

--- CameraCalibration/CameraCalibration.cpp
+++ CameraCalibration/CameraCalibration.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>  
-//#include <opencv2/core/core.hpp>
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
-using namespace std;
-using namespace cv;
-
-// I'm not using a header file to make the compilation easier, since it's just
-// an exercise, not a real program
-Mat getNextImage();
-int addChessboardPoints(Size& boardSize);
-void addPoints(const vector<Point2f>& imageCorners, const vector<Point3f>& objectCorners);
-double calibrate();
-void writeFile();
 
 // Number of image files to load from disk
 int nrImages; 
@@ -24,114 +13,91 @@ int nrFrames;
 // Index of current read image
 int currentImage;
 // Holds the image from image source
-Mat image, grayImage;
+cv::Mat image, grayImage;
 // Output Matrices
-Mat cameraMatrix; // Intrinsic parameters
-Mat distCoeffs;	  // Distortion coefficienst
+cv::Mat cameraMatrix; // Intrinsic parameters
+cv::Mat distCoeffs;	  // Distortion coefficienst
 // List of vectors with 2D points from the chessboard
-vector<vector<Point2f>> imagePoints;
+cv::vector<cv::vector<cv::Point2f>> imagePoints;
 // List of vectors with 3D points from the chessboard
-vector<vector<Point3f>> objectPoints;
+cv::vector<cv::vector<cv::Point3f>> objectPoints;
 //Output rotations and translations matrices
-vector<Mat> rvecs, tvecs;
+cv::vector<cv::Mat> rvecs, tvecs;
 
-
-int main() {
-
-	nrImages = 50;
-	nrFrames = 50;
-	currentImage = 1;
-
-	// 1. Find the corners on the chessboard
-	// Remember to count only the inner corners
-	addChessboardPoints(Size(10, 7));
-
-	// 2. Calibrate the camera based on the paramenters from the chessboard
-	calibrate();
+// Output the calibration result to a text file
+void writeFile(){
+		
+	std::ofstream calibrationResult;
+	calibrationResult.open ("calibration-result-luiz-sampaio.txt");
+	calibrationResult << "Intrinsic parameters: [a11] ... [a33] \n";
+	calibrationResult << std::fixed;
 	
-	// 3. Save the calibration result in a text file
-	writeFile();
-}
-
-// Get chessboard images and extract corner points
-int addChessboardPoints(Size& boardSize) {
-
-	// 2D position (image coordinate) of the corners for each image
-	vector<Point2f> imageCorners;  
-	// 3D position (world coordinate) of the corners for each image
-	vector<Point3f> objectCorners; 
-	
-	int success = 0;
-
-	// Initialize the vector with 3D points in arbitray measure unit
-	// The corners are at 3D location (X,Y,Z)= (i,j,0)
-	// Ex: <(0,0,0), (0,1,0), (0,2,0) ... (1,0,0), (1,1,0)...>
-	for (int i = 0; i < boardSize.height; i++) {
-		for (int j=0; j<boardSize.width; j++) {
-			objectCorners.push_back(Point3f(i, j, 0.0f));
+	for (int i = 0; i < cameraMatrix.rows; i++)
+	{
+		for (int j = 0; j < cameraMatrix.cols; j++)
+		{
+			calibrationResult << std::setprecision(6) << cameraMatrix.at<double>(i, j) << "\t";
 		}
 	}
+	
+	calibrationResult << "\n\n";
+	calibrationResult << "Extrinsic parameters : [r11] ... [r33][t1][t2][t3] \n";
 
-	while(success < nrFrames) {
+	for (int i = 0; i < (int)rvecs.size(); i++)
+	{
+		cv::Mat rotationMatix;
+		// Convert the rotation vector (3x1) to a rotation matrix (3x3)
+		Rodrigues(rvecs[i], rotationMatix);
 
-		image = getNextImage();
-		
-		// Convert the color image to grayscale
-		// in order to use the SubPix accuracy
-		cvtColor(image, grayImage, COLOR_BGR2GRAY);
+		calibrationResult << "[Image " << std::setw(2) << std::setfill('0') << i << "] "; 
 
-		// if we don't call the "waitKey", the imshow doesn't work
-		int key = waitKey(1);
-
-		// Get the chessboard corners. Possible filters to use
-		// CV_CALIB_CB_FAST_CHECK
-		//CV_CALIB_CB_ADAPTIVE_THRESH
-		//CV_CALIB_CB_NORMALIZE_IMAGE
-		//CV_CALIB_CB_FILTER_QUADS
-		bool found = findChessboardCorners(grayImage, boardSize, imageCorners);
-		
-		if(found){
-			// Get subpixel accuracy on the corners
-			cornerSubPix(grayImage, imageCorners, Size(5,5), Size(-1,-1), 
-				TermCriteria( TermCriteria::MAX_ITER + TermCriteria::EPS, 
-				30,	//30: max number of iterations 
-				0.1 //0.1: min accuracy
-				));     
-
-			drawChessboardCorners(image, boardSize, imageCorners, found);
-			
-			addPoints(imageCorners, objectCorners);
-			success++;
-
-			cout << "corners found! " << endl;
-			
+		// Writing the rotation matrix
+		for (int j = 0; j  < rotationMatix.rows; j++)
+		{
+			for (int k = 0; k < rotationMatix.cols; k++)
+			{
+				calibrationResult << std::setprecision(6) << rotationMatix.at<double>(j, k) << "\t";
+			}
 		}
-		else {
 
-			cout << "corners NOT found! " << endl;
+		// Writing the translation vector
+		for (int j = 0; j  < tvecs[i].rows; j++)
+		{
+			for (int k = 0; k < tvecs[i].cols; k++)
+			{
+				calibrationResult << std::setprecision(6) << tvecs[i].at<double>(j, k) << "\t";
+			}
 		}
-		
-		imshow("Corners on Chessboard", image);
+
+		calibrationResult << "\n";
 	}
-
-	return success;
+	
+	calibrationResult.close();
 }
 
-// Add scene points and corresponding image points
-void addPoints(const vector<Point2f>& imageCorners, const vector<Point3f>& objectCorners) {
+// Load chessboard images from disk
+cv::Mat getNextImage() {
 
-	// 2D image points from one view
-	imagePoints.push_back(imageCorners);          
-	// corresponding 3D scene points
-	objectPoints.push_back(objectCorners);
-	cout << "corners stored" << endl;
+	std::stringstream filename;
+	// Build the filename in the format provided
+	filename << "data/" << std::setw(5) << std::setfill('0')  << currentImage << ".jpg";
+
+	if(currentImage <= nrImages){
+	
+		currentImage++;
+	}
+
+	// Print as debug
+	std::cout << filename.str() << std::endl;
+
+	return cv::imread(filename.str());
 }
 
 // Calibrate the camera
 // returns the re-projection error
 double calibrate()
 {
-		cout << "Proceeding with calculations..." << endl;
+		std::cout << "Proceeding with calculations..." << std::endl;
 
 	// start calibration
 	return calibrateCamera(
@@ -147,71 +113,100 @@ double calibrate()
 	//,CV_CALIB_USE_INTRINSIC_GUESS);
 }
 
-// Load chessboard images from disk
-Mat getNextImage() {
+// Add scene points and corresponding image points
+void addPoints(const cv::vector<cv::Point2f>& imageCorners, const cv::vector<cv::Point3f>& objectCorners) {
 
-	stringstream filename;
-	// Build the filename in the format provided
-	filename << "data/" << setw(5) << setfill('0')  << currentImage << ".jpg";
-
-	if(currentImage <= nrImages){
-	
-		currentImage++;
-	}
-
-	// Print as debug
-	cout << filename.str() << std::endl;
-
-	return imread(filename.str());
+	// 2D image points from one view
+	imagePoints.push_back(imageCorners);          
+	// corresponding 3D scene points
+	objectPoints.push_back(objectCorners);
+	std::cout << "corners stored" << std::endl;
 }
 
-// Output the calibration result to a text file
-void writeFile(){
+// Get chessboard images and extract corner points
+int addChessboardPoints(cv::Size& boardSize) {
+
+	// 2D position (image coordinate) of the corners for each image
+	cv::vector<cv::Point2f> imageCorners;  
+	// 3D position (world coordinate) of the corners for each image
+	cv::vector<cv::Point3f> objectCorners; 
+	
+	int success = 0;
+
+	// Initialize the vector with 3D points in arbitray measure unit
+	// The corners are at 3D location (X,Y,Z)= (i,j,0)
+	// Ex: <(0,0,0), (0,1,0), (0,2,0) ... (1,0,0), (1,1,0)...>
+	for (int i = 0; i < boardSize.height; i++) {
+		for (int j=0; j<boardSize.width; j++) {
+			objectCorners.push_back(cv::Point3f(i, j, 0.0f));
+		}
+	}
+
+	while(success < nrFrames) {
+
+		image = getNextImage();
 		
-	ofstream calibrationResult;
-	calibrationResult.open ("calibration-result-luiz-sampaio.txt");
-	calibrationResult << "Intrinsic parameters: [a11] ... [a33] \n";
-	calibrationResult << fixed;
-	
-	for (int i = 0; i < cameraMatrix.rows; i++)
-	{
-		for (int j = 0; j < cameraMatrix.cols; j++)
-		{
-			calibrationResult << setprecision(6) << cameraMatrix.at<double>(i, j) << "\t";
+		// Convert the color image to grayscale
+		// in order to use the SubPix accuracy
+		cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+
+		// if we don't call the "waitKey", the imshow doesn't work
+		int key = cv::waitKey(1);
+
+		// Get the chessboard corners. Possible filters to use
+		// CV_CALIB_CB_FAST_CHECK
+		//CV_CALIB_CB_ADAPTIVE_THRESH
+		//CV_CALIB_CB_NORMALIZE_IMAGE
+		//CV_CALIB_CB_FILTER_QUADS
+		bool found = findChessboardCorners(grayImage, boardSize, imageCorners);
+		
+		if(found){
+			// Get subpixel accuracy on the corners
+			cornerSubPix(grayImage, imageCorners, cv::Size(5,5), cv::Size(-1,-1), 
+				cv::TermCriteria( cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 
+				30,	//30: max number of iterations 
+				0.1 //0.1: min accuracy
+				));     
+
+			drawChessboardCorners(image, boardSize, imageCorners, found);
+			
+			addPoints(imageCorners, objectCorners);
+			success++;
+
+			std::cout << "corners found! " << std::endl;
+			
 		}
+		else {
+
+			std::cout << "corners NOT found! " << std::endl;
+		}
+		
+		imshow("Corners on Chessboard", image);
 	}
-	
-	calibrationResult << "\n\n";
-	calibrationResult << "Extrinsic parameters : [r11] ... [r33][t1][t2][t3] \n";
 
-	for (int i = 0; i < (int)rvecs.size(); i++)
-	{
-		Mat rotationMatix;
-		// Convert the rotation vector (3x1) to a rotation matrix (3x3)
-		Rodrigues(rvecs[i], rotationMatix);
-
-		calibrationResult << "[Image " << setw(2) << setfill('0') << i << "] "; 
-
-		// Writing the rotation matrix
-		for (int j = 0; j  < rotationMatix.rows; j++)
-		{
-			for (int k = 0; k < rotationMatix.cols; k++)
-			{
-				calibrationResult << setprecision(6) << rotationMatix.at<double>(j, k) << "\t";
-			}
-		}
-
-		// Writing the translation vector
-		for (int j = 0; j  < tvecs[i].rows; j++)
-		{
-			for (int k = 0; k < tvecs[i].cols; k++)
-			{
-				calibrationResult << setprecision(6) << tvecs[i].at<double>(j, k) << "\t";
-			}
-		}
-
-		calibrationResult << "\n";
-	}
-	
-	calibrationResult.close();
+	return success;
 }
+
+int main() {
+
+	nrImages = 50;
+	nrFrames = 50;
+	currentImage = 1;
+
+	// 1. Find the corners on the chessboard
+	// Remember to count only the inner corners
+	addChessboardPoints(cv::Size(10, 7));
+
+	// 2. Calibrate the camera based on the paramenters from the chessboard
+	calibrate();
+	
+	// 3. Save the calibration result in a text file
+	writeFile();
+
+	return 0;
+}
+
+
+
+
+
